215c2c684ec1391c3519a5261229f1564d064caf
RitzyDevBox <RitzyDevBox@gmail.com>
2022-11-05 17:54:19 -0700

    Setting up test deployment contracts

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -4,16 +4,16 @@ import * as Marketplaces from '../lib/Marketplaces';
 import whitelistAddresses from './whitelist.json';
 
 const CollectionConfig: CollectionConfigInterface = {
-  testnet: Networks.ethereumTestnet,
-  mainnet: Networks.ethereumMainnet,
+  testnet: Networks.polygonTestnet,
+  mainnet: Networks.polygonMainnet,
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  contractName: 'MonstarsNFT',
+  tokenName: 'Monstars',
+  tokenSymbol: 'MSTR',
+  hiddenMetadataUri: 'ipfs://QmTtXFZiMLf57mKRkBBZYJWSdNKekTej8CES3CLGNV1GfH/hidden.json',
+  maxSupply: 21,
   whitelistSale: {
     price: 0.05,
     maxMintAmountPerTx: 1,
@@ -26,7 +26,7 @@ const CollectionConfig: CollectionConfigInterface = {
     price: 0.09,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: null,
+  contractAddress: '0xD49794c90aBeD10757CCC82c43eCF5a24c2e6557',
   marketplaceIdentifier: 'my-nft-token',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
